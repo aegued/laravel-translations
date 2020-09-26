c363470e9253d474ba91162bae2a90b7dabf0018
Alfredo <egued89@gmail.com>
2020-09-26 11:48:28 +0200

    Init

---
+++ src/LaravelTranslationsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aegued\LaravelTranslations;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTranslationsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadM;
+    }
+}
